0cc16ad37ff079d7ec69e296a84353c37b177c74
thanhtnguyen <thanhtnguyen@paypal.com>
2014-10-24 03:32:26 +0800

    Add install file

---
+++ install.sh
@@ -0,0 +1,18 @@
+# First, install necessary apps:
+
+# Install brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Copy dot files
+cp aliases ~/.aliases
+cp bash_profile ~/.bash_profile
+cp bash_prompt ~/.bash_prompt
+cp bashrc ~/.bashrc
+cp curlrc ~/.curlrc
+cp exports ~/.exports
+cp extra ~/.extra
+cp functions ~/.functions
+cp osx ~/.osx
+cp profile ~/.profile
+cp screenrc ~/.screenrc
+cp vimrc ~/.vimrc
