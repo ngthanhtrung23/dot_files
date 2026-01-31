d049701398a73e7677dd387a27ed179a7c45bf7d
Trung Nguyen <ngthanhtrung23@gmail.com>
2026-01-31 15:54:08 +0800

    goto next/prev error & show error

--- config_nvim_init.lua
+++ config_nvim_init.lua
@@ -191,8 +191,9 @@ require("lazy").setup({
           vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
           vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
           vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+          vim.keymap.set('n', '[e', vim.diagnostic.goto_prev, opts)
+          vim.keymap.set('n', ']e', vim.diagnostic.goto_next, opts)
+          vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
         end,
       })
     end,
